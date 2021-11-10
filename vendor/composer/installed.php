9219ca26881f899b0cd2fb065eb477d8e70e0952
Jaqueline-Costa <70847791+Jaqueline-Costa@users.noreply.github.com>
2021-11-10 00:19:19 -0300

    Atualização ext

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => 'bb76759647c62f09eb230dcd5b6f2aa62d00e489',
+        'reference' => 'b27f51ab646469f964c4850827dd34780b5ce1ce',
         'name' => 'laravel/laravel',
         'dev' => true,
     ),
@@ -436,7 +436,7 @@
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => 'bb76759647c62f09eb230dcd5b6f2aa62d00e489',
+            'reference' => 'b27f51ab646469f964c4850827dd34780b5ce1ce',
             'dev_requirement' => false,
         ),
         'laravel/sail' => array(
@@ -823,8 +823,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0',
-                1 => '1.0.0 || 2.0.0 || 3.0.0',
+                0 => '1.0.0 || 2.0.0 || 3.0.0',
+                1 => '1.0|2.0',
             ),
         ),
         'psr/simple-cache' => array(
